aa523f8975d74e199e96dc98e7c95984e0b6ee91
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-23 19:43:53 -0400

    add clang-format & gitignores

--- src/Device.h
+++ src/Device.h
@@ -1,30 +1,32 @@
 #pragma once
 
+#include <vulkan/vulkan.h>
+
 #include <array>
 #include <stdexcept>
-#include <vulkan/vulkan.h>
+
 #include "QueueFlags.h"
 #include "SwapChain.h"
 
 class SwapChain;
 class Device {
-    friend class Instance;
+  friend class Instance;
 
 public:
-    SwapChain* CreateSwapChain(VkSurfaceKHR surface, unsigned int numBuffers);
-    Instance* GetInstance();
-    VkDevice GetVkDevice();
-    VkQueue GetQueue(QueueFlags flag);
-    unsigned int GetQueueIndex(QueueFlags flag);
-    ~Device();
+  SwapChain* CreateSwapChain(VkSurfaceKHR surface, unsigned int numBuffers);
+  Instance* GetInstance();
+  VkDevice GetVkDevice();
+  VkQueue GetQueue(QueueFlags flag);
+  unsigned int GetQueueIndex(QueueFlags flag);
+  ~Device();
 
 private:
-    using Queues = std::array<VkQueue, sizeof(QueueFlags)>;
-    
-    Device() = delete;
-    Device(Instance* instance, VkDevice vkDevice, Queues queues);
+  using Queues = std::array<VkQueue, sizeof(QueueFlags)>;
 
-    Instance* instance;
-    VkDevice vkDevice;
-    Queues queues;
+  Device() = delete;
+  Device(Instance* instance, VkDevice vkDevice, Queues queues);
+
+  Instance* instance;
+  VkDevice vkDevice;
+  Queues queues;
 };
