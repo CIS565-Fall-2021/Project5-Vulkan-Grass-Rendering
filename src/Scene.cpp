4623b4192ca2ad3c6e18df9df4e915cbe8e166c1
Aditya Hota <12516225+adityahota@users.noreply.github.com>
2021-11-02 21:30:34 -0400

    Add profiling logic

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,6 +1,8 @@
 #include "Scene.h"
 #include "BufferUtils.h"
 
+#define FPS_FRAME_PERIOD 20
+
 Scene::Scene(Device* device) : device(device) {
     BufferUtils::CreateBuffer(device, sizeof(Time), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, timeBuffer, timeBufferMemory);
     vkMapMemory(device->GetVkDevice(), timeBufferMemory, 0, sizeof(Time), 0, &mappedData);
@@ -24,9 +26,25 @@ void Scene::AddBlades(Blades* blades) {
 }
 
 void Scene::UpdateTime() {
+    static unsigned int frames_count = 0;
+    static float last_frames_time = 0.f;
+
     high_resolution_clock::time_point currentTime = high_resolution_clock::now();
     duration<float> nextDeltaTime = duration_cast<duration<float>>(currentTime - startTime);
     startTime = currentTime;
+
+    frames_count++;
+    last_frames_time += duration_cast<std::chrono::milliseconds>(nextDeltaTime).count();
+
+    if (frames_count == FPS_FRAME_PERIOD)
+    {
+        float fps =  FPS_FRAME_PERIOD * 1000 / last_frames_time;
+        std::cout << fps << std::endl;
+
+        frames_count = 0;
+        last_frames_time = 0.f;
+    }
+
 
     time.deltaTime = nextDeltaTime.count();
     time.totalTime += time.deltaTime;
